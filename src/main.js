56147becede3aac5eba7f0080c10df945a2e1d09
ValmurRU <109274645+ValmurRU@users.noreply.github.com>
2022-12-08 15:58:48 +0300

    init

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-import '@/assets/style/reset.min.css';
-import '@/assets/style/keyframes.scss';
-import '@/assets/style/animation.scss';
-
-createApp(App).mount('#app');
